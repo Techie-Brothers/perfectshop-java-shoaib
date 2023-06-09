744f4ae9b820f92e99816094866386bd1c1a7e9d
shoaib-se <shoaibkhan.se@gmail.com>
2023-06-09 17:47:30 +0530

    Initial Commit.

---
+++ src/main/java/com/perfectshop/PerfectShopApplication.java
@@ -0,0 +1,13 @@
+package com.perfectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfectshop/PerfectShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
